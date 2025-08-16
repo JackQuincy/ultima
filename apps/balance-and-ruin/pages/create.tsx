0e5dcc5712dc962e2626aaefe922c5e8c613119a
Jack <JackQuincy@users.noreply.github.com>
2025-08-16 14:09:33 -0700

    Add preset query string parameter

--- apps/balance-and-ruin/pages/create.tsx
+++ apps/balance-and-ruin/pages/create.tsx
@@ -15,6 +15,16 @@ export type PageProps = {
   schema: Record<string, RawFlagMetadata>;
 };
 
+const DecodeB64QueryStringParam  = (param: string) => {
+  let base64 = param.replace(/-/g, '+').replace(/_/g, '/');
+  // Add padding if necessary
+  while (base64.length % 4 !== 0) {
+    base64 += '=';
+  }
+  const buf = Buffer.from(base64, "base64")
+  return buf.toString("utf-8")
+}
+
 const Create = () => {
 
   const [objectives, setObjectives] = useState(null)
@@ -32,18 +42,19 @@ const Create = () => {
         
         const queryParameters = new URLSearchParams(window.location.search)
         const flagsParam = queryParameters.get("flags")
+        const presetParam = queryParameters.get("preset")
         if(flagsParam) {
-          let base64 = flagsParam.replace(/-/g, '+').replace(/_/g, '/');
-          // Add padding if necessary
-          while (base64.length % 4 !== 0) {
-            base64 += '=';
-          }
-          const buf = Buffer.from(base64, "base64")
-          const flags = buf.toString("utf-8")
+          const flags = DecodeB64QueryStringParam(flagsParam)
           console.log("Setting starting flags from query string")
           store.dispatch(setRawFlags(flags));
         } else {
-          const preset = data["ultros league"];
+          let preset: FlagPreset
+          if (presetParam) {
+            const presetName = DecodeB64QueryStringParam(presetParam)
+            preset = data[presetName];
+          } else {
+            preset = data["ultros league"];
+          }
           if (preset) {
             store.dispatch(setRawFlags(preset.flags));
           }
